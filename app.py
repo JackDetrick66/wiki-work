52febff8dd3c7b7cccb734b77f6db76745c05351
Jack Detrick <98991308+JackDetrick66@users.noreply.github.com>
2025-05-13 16:59:45 -0400

    utilized flask more
    also organized files

---
+++ app.py
@@ -0,0 +1,18 @@
+# app.py
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/contentListing')
+def contentListing():
+    return render_template('contentListing.html')
+
+if __name__=='__main__': 
+   app.run(debug=True) 
--- static/js_front.js
+++ static/js_front.js
@@ -6,6 +6,14 @@ window.alert("BEING REROUTED, GOOD LUCK");
 window.location.href='https://www.google.com/'
 };
 
+function enteringArchiv(){
+window.alert('Hello Archivist! Remain Vigilant Of Your Surroundings At All Times When Accessing The Archives!');
+
+}
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("enteringArchiv").addEventListener("click", enteringArchiv);
+});
+
 // Wait until the DOM is fully loaded, then attach the event listener
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("defMeasure").addEventListener("click", defMeasureFunc);
